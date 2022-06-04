eb4abfbede2b84453ba469b7c831f997c7ded4ef
Angelo GALLO <gallo.angelo.info@gmail.com>
2022-06-04 16:33:33 +0200

    feat(AppointmentList): add practitioner & patient filters

--- src/components/AppointmentList.tsx
+++ src/components/AppointmentList.tsx
@@ -3,7 +3,7 @@ import appointmentsSlice, {
   getAppointments,
 } from 'store/appointments';
 import { useDispatch, useSelector } from 'react-redux';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
   Card,
   CardActionArea,
@@ -11,6 +11,8 @@ import {
   Typography,
   CardActions,
   Button,
+  TextField,
+  Grid,
 } from '@material-ui/core';
 import { getPatients, patientsSelectors } from 'store/patients';
 import { getPractitioners, practitionersSelectors } from 'store/practitioners';
@@ -18,20 +20,61 @@ import { formatDateRange } from 'utils/date';
 
 const AppointmentList = () => {
   const dispatch = useDispatch();
-  const appointments = useSelector((state) =>
-    appointmentsSelectors.selectAll(state.appointments),
-  );
-  const practitioners = useSelector((state) =>
-    practitionersSelectors.selectAll(state.practitioners),
-  );
-  const patients = useSelector((state) =>
-    patientsSelectors.selectAll(state.patients),
-  );
+
   useEffect(() => {
     dispatch(getPractitioners());
     dispatch(getPatients());
     dispatch(getAppointments());
   }, [dispatch]);
+
+  const [filters, setFilters] = useState({
+    practitioner: undefined,
+    patient: undefined,
+  });
+
+  const practitioners = useSelector((state) =>
+    practitionersSelectors.selectAll(state.practitioners),
+  );
+
+  const patients = useSelector((state) =>
+    patientsSelectors.selectAll(state.patients),
+  );
+
+  const appointments = useSelector((state) => {
+    // practitioner filter
+    let list = appointmentsSelectors
+      .selectAll(state.appointments)
+      .filter((item) => {
+        if (!filters.practitioner || filters.practitioner === '') {
+          return true;
+        } else {
+          const practitioner = practitioners.find(
+            (p) => p.id === item.practitionerId,
+          );
+          if (practitioner) {
+            const fullName = `${practitioner.firstName} ${practitioner.lastName}`.toLowerCase();
+            return fullName.includes(filters.practitioner.toLowerCase());
+          }
+          return false;
+        }
+      });
+
+    // patient filter
+    list = list.filter((item) => {
+      if (!filters.patient || filters.patient === '') {
+        return true;
+      } else {
+        const patient = patients.find((p) => p.id === item.patientId);
+        if (patient) {
+          const fullName = `${patient.firstName} ${patients.lastName}`.toLowerCase();
+          return fullName.includes(filters.patient.toLowerCase());
+        }
+        return false;
+      }
+    });
+
+    return list;
+  });
 
   const handleDelete = useCallback(
     (id: number) => {
@@ -42,6 +85,29 @@ const AppointmentList = () => {
 
   return (
     <div className="appointment__list__container">
+      <div className="appointment__list__container__filters">
+        <Typography variant="h5">Filters</Typography>
+        <Grid container spacing={2}>
+          <Grid item>
+            <TextField
+              label="Practitioner"
+              variant="outlined"
+              onChange={(e) =>
+                setFilters((old) => ({ ...old, practitioner: e.target.value }))
+              }
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Patient"
+              variant="outlined"
+              onChange={(e) =>
+                setFilters((old) => ({ ...old, patient: e.target.value }))
+              }
+            />
+          </Grid>
+        </Grid>
+      </div>
       <div className="appointment__list__container__items">
         {appointments.map((appointment) => {
           const practitioner = practitioners.find(
